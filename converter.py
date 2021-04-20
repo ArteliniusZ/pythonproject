82b59301d38adcb6a20b696ea54232368903a50a
ArteliniusZ <azgardan.908@gmail.com>
2021-04-20 20:30:11 +0300

    day 2: exercises

---
+++ converter.py
@@ -0,0 +1,6 @@
+print("Enter temperature value in Celsius")
+celsius_value = input()
+celsius_value = float(celsius_value)
+celsius_value_fahr = (celsius_value * 9/5) + 32
+print(f"Kelvin value is: {celsius_value + 273}")
+print("Fahrenheit value is %.2f" %(celsius_value_fahr))
