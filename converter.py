e04418debfaa581d3f5d6a3a6e1c9763a02cd920
ArteliniusZ <azgardan.908@gmail.com>
2021-04-22 20:30:37 +0300

    day 3: exercises

--- converter.py
+++ converter.py
@@ -3,4 +3,4 @@ celsius_value = input()
 celsius_value = float(celsius_value)
 celsius_value_fahr = (celsius_value * 9/5) + 32
 print(f"Kelvin value is: {celsius_value + 273}")
-print("Fahrenheit value is %.2f" %(celsius_value_fahr))
+print("Fahrenheit value is: %.2f" %celsius_value_fahr)
--- main.py
+++ main.py
@@ -3,8 +3,30 @@ password = "hackme";
 
 print("This program checks if username and password are correct\n");
 
-input_username = input ("Please enter username: ");
+input_username = input("Please enter username: ");
 input_password = input("Please enter password: ");
+is_username_valid = username == input_username
+is_password_valid = password == input_password
 
-print(username == input_username);
-print(password == input_password);
+if not (is_username_valid and is_password_valid):
+    if not is_username_valid:
+        print("Invalid username")
+    if not is_password_valid:
+        print("Invalid password")
+    exit(1)
+
+if is_username_valid and is_password_valid:
+    print("Welcome " + input_username + ", you are authorized")
+    shut_down = input("Do you want to shut down the computer?: ")
+    if shut_down == 'yes':
+        print("Have a nice day!")
+    elif shut_down == 'no':
+        admin = input("Do you want to become an admin? ")
+        if admin == 'yes':
+            print("Nice!")
+        elif admin == 'no':
+            print("OK")
+        else:
+            print("What?")
+else:
+    print("Have a nice day!")
